285f1aec86dc1059d7b5071c2a07dd70586c451d
Ken C.Y. Leung <kenleung5e28@gmail.com>
2022-03-12 14:46:46 +0800

    implement get_args

--- src/lib.rs
+++ src/lib.rs
@@ -32,22 +32,6 @@ pub fn get_args() -> MyResult<Config> {
             .multiple(true)
         )
         .arg(
-            Arg::with_name("bytes")
-            .value_name("BYTES")
-            .short("b")
-            .long("bytes")
-            .help("Selected bytes")
-            .takes_value(true)
-        )
-        .arg(
-            Arg::with_name("chars")
-            .value_name("CHARS")
-            .short("c")
-            .long("chars")
-            .help("Selected characters")
-            .takes_value(true)
-        )
-        .arg(
             Arg::with_name("delimiter")
             .value_name("DELIMITER")
             .short("d")
@@ -56,17 +40,58 @@ pub fn get_args() -> MyResult<Config> {
             .default_value("\t")
         )
         .arg(
+            Arg::with_name("bytes")
+            .value_name("BYTES")
+            .short("b")
+            .long("bytes")
+            .help("Selected bytes")
+            .takes_value(true)
+            .conflicts_with_all(&["chars", "fields"])
+            .required_unless_one(&["chars", "fields"])
+        )
+        .arg(
+            Arg::with_name("chars")
+            .value_name("CHARS")
+            .short("c")
+            .long("chars")
+            .help("Selected characters")
+            .takes_value(true)
+            .conflicts_with("fields")
+            .required_unless_one(&["bytes", "fields"])
+        )
+        .arg(
             Arg::with_name("fields")
             .value_name("FIELDS")
             .short("f")
             .long("fields")
             .help("Selected fields")
             .takes_value(true)
+            .required_unless_one(&["bytes", "chars"])
         )
         .get_matches();
+    let delimiter = matches.value_of("delim").unwrap();
+    if delimiter.len() != 1 {
+        return Err(From::from(format!("--delim \"{}\" must be a single byte", delimiter)));
+    }
+    let extract = if matches.is_present("bytes") {
+        Bytes(parse_pos(matches.value_of("bytes").unwrap())?)
+    } else if matches.is_present("chars") {
+        Chars(parse_pos(matches.value_of("chars").unwrap())?)
+    } else {
+        Fields(parse_pos(matches.value_of("fields").unwrap())?)
+    };
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        delimiter: delimiter.bytes().nth(0).unwrap(),
+        extract,
+    })
 }
 
 pub fn run(config: Config) -> MyResult<()> {
     println!("{:#?}", config);
     Ok(())
+}
+
+fn parse_pos(range: &str) -> MyResult<PositionList> {
+    unimplemented!();
 }
